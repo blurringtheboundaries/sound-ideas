2f4bea285d636d5636c6882457b552751693e760
Charles Matthews <charles0matthews@gmail.com>
2023-02-27 15:05:19 -0500

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = dirname(__filename);
+
+export default {
+
+    entry: resolve(__dirname, 'src/index.js'),
+    output: {
+        filename: 'sound-ideas-main.js',
+        path: resolve(__dirname, 'dist'),
+        library: 'SoundIdeas',
+        libraryTarget: 'umd'
+    },
+    module:{
+        rules: [
+            {
+                test:/\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    mode:'production'
+}
