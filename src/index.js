47c3110fcc0aef555e14b3ae68123989ee210330
Charles Matthews <charles0matthews@gmail.com>
2023-03-01 11:41:21 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const loader = ()=>{
     const links = document.querySelectorAll("a.link");
     links.forEach((link) => {
         // add an icon to the end of the link -- no alt text because the link element is sufficient
-        link.insertAdjacentHTML("beforeend", `<img src="icons/noun-link-1047903-FFFFFF.svg" style="width:1em; height:1em;" aria-hidden="true">`);
+        link.insertAdjacentHTML("beforeend", `<img src="icons/noun-link-1047903-FFFFFF.svg" class="main__link__icon" style="width:1em; height:1em;" aria-hidden="true">`);
     });
 }
 
