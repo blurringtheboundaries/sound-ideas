c44ee7ee6e30fc5015d4156dbdfc51ec620b9f84
Charles Matthews <charles0matthews@gmail.com>
2023-03-01 12:02:30 -0500

    automate link colouring

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ const loader = ()=>{
     const links = document.querySelectorAll("a.link");
     links.forEach((link) => {
         // add an icon to the end of the link -- no alt text because the link element is sufficient
-        link.insertAdjacentHTML("beforeend", `<img src="icons/noun-link-1047903-FFFFFF.svg" class="main__link__icon" style="width:1em; height:1em;" aria-hidden="true">`);
+        link.insertAdjacentHTML("beforeend", `<object data="icons/noun-link-1047903-FFFFFF.svg" class="main__link__icon" style="width:1em; height:1em;" aria-hidden="true">`);
+        let color = getComputedStyle(link).getPropertyValue('color');
+        link.querySelector("object").addEventListener("load", (e) => {
+     
+                e.target.contentDocument.querySelectorAll('g').forEach(y=>y.setAttribute('stroke',color))
+        });
     });
 }
 
