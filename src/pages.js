08efad54f42d126c32ca71ecbefff8f536edaa3d
Charles Matthews <charles0matthews@gmail.com>
2023-03-02 11:36:13 -0500

    add chord player, refine qr layout

--- src/pages.js
+++ src/pages.js
@@ -1,4 +1,5 @@
 export const pages = {
     'index': 'https://blurringtheboundaries.github.io/sound-ideas/index.html',
-    'note-player': 'https://blurringtheboundaries.github.io/sound-ideas/index.html'
+    'note-player': 'https://blurringtheboundaries.github.io/sound-ideas/index.html',
+    'chord-player': 'https://blurringtheboundaries.github.io/sound-ideas/index.html'
 }
