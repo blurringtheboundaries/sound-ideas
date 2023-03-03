8d0e66e0c4b895510e0b6a9b1b38fec956cfa8a1
Charles Matthews <charles0matthews@gmail.com>
2023-03-03 14:44:40 -0500

    fixing issue with touch on main page

--- src/pages.js
+++ src/pages.js
@@ -1,5 +1,7 @@
 export const pages = {
     'index': 'https://blurringtheboundaries.github.io/sound-ideas/index.html',
     'note-player': 'https://blurringtheboundaries.github.io/sound-ideas/note-player.html',
-    'chord-player': 'https://blurringtheboundaries.github.io/sound-ideas/chord-player.html'
+    // 'chord-player': 'https://blurringtheboundaries.github.io/sound-ideas/chord-player.html',
+    'chord-player': 'https://blurringtheboundaries.github.io/sound-ideas/embed-demo.html'
+
 }
