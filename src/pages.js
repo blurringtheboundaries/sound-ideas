40d230e5905c2b5cbde5cb0398c5dd30a281b2d2
Charles Matthews <charles0matthews@gmail.com>
2023-04-30 08:22:53 -0400

    update note player page from index

--- src/pages.js
+++ src/pages.js
@@ -1,6 +1,6 @@
 export const pages = {
     'index': 'https://blurringtheboundaries.github.io/sound-ideas/index.html',
-    'note-player': 'https://blurringtheboundaries.github.io/sound-ideas/note-player',
+    'note-player': 'https://blurringtheboundaries.github.io/sound-ideas/embed-copper.html',
     // 'chord-player': 'https://blurringtheboundaries.github.io/sound-ideas/chord-player.html',
     'chord-player': 'https://blurringtheboundaries.github.io/sound-ideas/embed-demo.html'
 
