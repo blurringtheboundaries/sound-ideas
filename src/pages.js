14abd031d69049bcfcea58b984946a4fd94f0a3c
Charles Matthews <charles0matthews@gmail.com>
2023-07-02 13:16:56 -0400

    add faders to gallery

--- src/pages.js
+++ src/pages.js
@@ -2,6 +2,7 @@ export const pages = {
     'index': 'https://blurringtheboundaries.github.io/sound-ideas/index.html',
     'note-player': 'https://blurringtheboundaries.github.io/sound-ideas/embed-copper.html',
     // 'chord-player': 'https://blurringtheboundaries.github.io/sound-ideas/chord-player.html',
-    'chord-player': 'https://blurringtheboundaries.github.io/sound-ideas/embed-demo.html'
+    'chord-player': 'https://blurringtheboundaries.github.io/sound-ideas/embed-demo.html',
+    'faders': 'https://blurringtheboundaries.github.io/sound-ideas/fader.html',
 
 }
