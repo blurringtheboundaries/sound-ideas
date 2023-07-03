c80abe3fd20933b675781846bfb3c269e299406c
Charles Matthews <charles0matthews@gmail.com>
2023-07-03 16:56:24 -0400

    add 'external' folder from previous sketch
    just to get this up and running quickly...

---
+++ docs/external/shortcuts.js
@@ -0,0 +1,5 @@
+const query = document.querySelector.bind(document);
+const queryAll = document.querySelectorAll.bind(document);
+const fromId = document.getElementById.bind(document);
+const fromClass = document.getElementsByClassName.bind(document);
+const fromTag = document.getElementsByTagName.bind(document);
