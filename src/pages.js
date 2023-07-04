dea886959b810f2317099262ff1981c0f524b52f
Charles Matthews <charles0matthews@gmail.com>
2023-07-04 14:43:38 -0400

    add chord sequencer to gallery
    not quite ready, but good enough to demonstrate the concept.

--- src/pages.js
+++ src/pages.js
@@ -4,5 +4,6 @@ export const pages = {
     // 'chord-player': 'https://blurringtheboundaries.github.io/sound-ideas/chord-player.html',
     'chord-player': 'https://blurringtheboundaries.github.io/sound-ideas/embed-demo.html',
     'faders': 'https://blurringtheboundaries.github.io/sound-ideas/fader.html',
-    'custom': 'https://blurringtheboundaries.github.io/sound-ideas/setup.html'
+    'custom': 'https://blurringtheboundaries.github.io/sound-ideas/setup.html',
+    'chord-sequencer': 'https://blurringtheboundaries.github.io/sound-ideas/chord-sequencer.html'
 }
