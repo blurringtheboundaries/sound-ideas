8a74a430e14e070072c582e3774900c7d358e825
Charles Matthews <charles0matthews@gmail.com>
2023-11-26 18:30:51 -0500

    testing presets

--- src/synthHandler.js
+++ src/synthHandler.js
@@ -1,77 +1,96 @@
 export const synthHandler = {
-    presets:{
-        plink:{
-            filter:{
-                type:'highpass',
-                frequency:100
-            },
-            synth:{
-                oscillator:{
-                    type:'sine4',
-                    volume:-3
-                },
-                envelope:{
-                    attack:0.01,
-                    decay:0.2,
-                    release:0.1
-                }
-            }
+  presets: {
+    plink: {
+      filter: {
+        type: "highpass",
+        frequency: 100,
+      },
+      synth: {
+        oscillator: {
+          type: "sine4",
+          volume: -3,
         },
-        soft:{
-            filter:{
-                type:'lowpass',
-                frequency:10000
-            },
-            synth:{
-                oscillator:{
-                    type:'sine2', 
-                    volume:-6,
-                    detune:Math.random()-0.5
-                },
-                envelope:{
-                    attack:1,
-                    release:4
-                }
-            }
-            
+        envelope: {
+          attack: 0.01,
+          decay: 0.2,
+          release: 0.1,
         },
-        plunk:{
-            filter:{
-                type:'lowpass',
-                frequency:2000
-            },
-            synth:{
-                oscillator:{
-                    type:'sawtooth3',
-                    volume:-24
-                },
-                envelope:{
-                    attack:0.01,
-                    decay:0.4,
-                    release:2
-                }
-            }
+      },
+    },
+    soft: {
+      filter: {
+        type: "lowpass",
+        frequency: 10000,
+      },
+      synth: {
+        oscillator: {
+          type: "sine2",
+          volume: -6,
+          detune: Math.random() - 0.5,
         },
-    }
-}
+        envelope: {
+          attack: 1,
+          release: 4,
+        },
+      },
+    },
+    plunk: {
+      filter: {
+        type: "lowpass",
+        frequency: 2000,
+      },
+      synth: {
+        oscillator: {
+          type: "sawtooth3",
+          volume: -24,
+        },
+        envelope: {
+          attack: 0.01,
+          decay: 0.4,
+          release: 2,
+        },
+      },
+    },
+    test: {
+      filter: {
+        type: "highpass",
+        frequency: 100,
+      },
+      synth: {
+        oscillator: {
+          type: "sine4",
+          volume: -3,
+        },
+        envelope: {
+          attack: 0.01,
+          decay: 0.2,
+          release: 0.1,
+        },
+      },
+    },
+  },
+};
 
-if(!localStorage.getItem('synthPresets')){
-    localStorage.setItem('synthPresets',JSON.stringify(synthHandler.presets))
+if (!localStorage.getItem("synthPresets")) {
+  localStorage.setItem("synthPresets", JSON.stringify(synthHandler.presets));
 } else {
-    synthHandler.presets = JSON.parse(localStorage.getItem('synthPresets'));
+  synthHandler.presets = JSON.parse(localStorage.getItem("synthPresets"));
 }
 
 let synthPresets = synthHandler.presets;
 
-if (document.querySelector('#select_preset')){
-    Object.keys(synthPresets).forEach(x=>{
-        document.querySelector('#select_preset').appendChild(cm.create('option',{
-            innerHTML:x 
-        }))  
-    })
-    
-    document.querySelector('#select_preset').addEventListener('input',function(){
-        instrument.setParameters(synthPresets[this.value]);
-    })
-};
+if (document.querySelector("#select_preset")) {
+  Object.keys(synthPresets).forEach((x) => {
+    document.querySelector("#select_preset").appendChild(
+      cm.create("option", {
+        innerHTML: x,
+      })
+    );
+  });
 
+  document
+    .querySelector("#select_preset")
+    .addEventListener("input", function () {
+      instrument.setParameters(synthPresets[this.value]);
+    });
+}
